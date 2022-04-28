fd7ba0a49f5817a691605fb663cd2b2bc2357d55
minakshid6 <104327664+minakshid6@users.noreply.github.com>
2022-04-28 01:05:19 -0500

    it ok

---
+++ IT Company name/src/names/NameIT.java
@@ -0,0 +1,12 @@
+package names;
+
+public class NameIT {
+
+	public static void main(String[] args) {
+		System.out.println("Cognizant");
+		System.out.println("IBM");
+		System.out.println("Wipro");
+
+	}
+
+}
